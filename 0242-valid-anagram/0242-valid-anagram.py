42fe8bc8f58719e8b11a41ac1f6f8bc581a06ff2
Abdulrahman Awad <113211338+AbduAwad@users.noreply.github.com>
2023-09-02 18:34:01 -0400

    Time: 34 ms (71.12%), Space: 15.2 MB (12.95%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if (len(s) != len(t)):
+            return False
+        
+        countS = {}
+        countT = {}
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        return countS == countT
