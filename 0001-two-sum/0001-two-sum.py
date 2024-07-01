2acbbaab1cf300a7e691f2ff0a22f47c00f5400d
Abdulrahman Awad <113211338+AbduAwad@users.noreply.github.com>
2024-07-01 13:02:01 -0400

    Time: 26 ms (99.23%), Space: 12.4 MB (88.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        num_indices = {}  # Dictionary to store elements and their indices
-    
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_indices:
-                return [num_indices[complement], i]
-            num_indices[num] = i
-            
-        # If no solution is found, return an empty list
-        return []
+        numsMap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if (numsMap.get(complement) != None):
+                return [i, numsMap.get(complement)]
+            numsMap[nums[i]] = i
