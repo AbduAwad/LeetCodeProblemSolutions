e7e5ff39909446bb5349fd704380f07ffc62088a
Abdulrahman Awad <113211338+AbduAwad@users.noreply.github.com>
2024-08-22 12:27:38 -0400

    Time: 41 ms (14.09%), Space: 16.4 MB (58.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -3,7 +3,11 @@ class Solution:
         binaryNum = str(bin(num)[2:])
         binLength = len(binaryNum)
         answer = 0
-        for i in range(binLength - 1, -1, -1):
-            if binaryNum[i] == '0':
-                answer += (2**(binLength - i - 1))
+        # for i in range(binLength - 1, -1, -1):
+        #     if binaryNum[i] == '0':
+        #         answer += (2**(binLength - i - 1))
+                
+        for i in range(binLength):
+            if (binaryNum[binLength - i - 1] == '0'):
+                answer += 2**i
         return answer
